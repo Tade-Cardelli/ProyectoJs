695b6224590d7716a7027202cfa539a4d4391a45
Tadeo Cardelli <tadeocardelli@hotmail.com>
2022-08-25 18:32:50 -0300

    ciclos

---
+++ app.js
@@ -0,0 +1,7 @@
+let nota1 = prompt("Ingrese la nota del primer mes");
+let nota2 = prompt("Ingrese su segunda nota");
+let nota3 = prompt("Ingrese la tercera nota");
+
+let promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) /3;
+
+alert(promedio);
---
+++ app2.js
@@ -0,0 +1,13 @@
+//ejemplo 1
+let numero = prompt("Ingrese un numero");
+
+for (let x = 1; x <=numero; x++){
+    console.log(`Numero ${x}`);
+}
+
+//ejemplos 2
+
+for (let x = 1; x <= 5; x++){
+    var nombre = prompt("Ingerse un nombre");
+    console.log(`Los nombres son ${nombre}`)
+}
