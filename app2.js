882a58bf78c70dec7591881010c8a85c80a7c235
Tadeo Cardelli <tadeocardelli@hotmail.com>
2022-09-06 21:08:15 -0300

    arrays

--- app2.js
+++ app2.js
@@ -1,15 +1,21 @@
-//ejemplo 1
-let numero = prompt("Ingrese un numero");
+const productos = [
+    {nombre:"teclado", precio: 1000},
+    {nombre:"mouse", precio: 850},
+    {nombre:"auriculares", precio:2000},
+    {nombre:"monitor", precio:5000},
+];
+let carrito = []
 
-for (let x = 1; x <=numero; x++){
-    console.log(`Numero ${x}`);
+let opcion = prompt("Buenas, desea comprar algun componente- si/no")
+
+while(opcion != "si" && opcion != "no"){
+    alert("La palabra ingresada es incorrecta, por favor ingrese si/no")
+    opcion = prompt("Buenas, desea comprar algun componente- si/no")
 }
-
-//ejemplos 2
-
-for (let x = 1; x <= 5; x++){
-    var nombre = prompt("Ingerse un nombre");
-    console.log(`Los nombres son ${nombre}`)
+if(opcion == "si");{
+    alert("Lista de productos")
+    let todosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+    alert(todosProductos.join("-"))
+}   if(opcion == "no"){
+    alert("Gracias por ver, hasta luego")
 }
-
-//ejemplo con ifs
