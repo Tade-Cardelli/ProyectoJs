d13705c801dd1f2938a00d0934a0df734ecd7a77
Tadeo Cardelli <tadeocardelli@hotmail.com>
2022-09-15 14:56:54 -0300

    Eventos

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+productos = [
+    {id:1, nombre:"Curso hmtl", precio:"$20.000",},
+    {id:2, nombre:"Curso Css", precio:"$50.000", },
+    {id:3, nombre:"Curso JS", precio:"$50.000"}
+]
 
-function impuesto(){
-    let precio = prompt("Ingrese un precio para calcular el IVA o escriba ESC para salir")
-    while (precio != "ESC" && precio != "esc"){
-        let iva = (precio * 21)/100;
-        let total = Number(precio) + iva;
-        console.log(iva);
-        console.log(total);
-        alert(`El impuesto es de ${iva} pesos adicionales y el total es de ${Number (total)}`)
-        break
-    }
-}
+carrito = []
 
-impuesto();
+const boton1 = document.querySelector("button")
+const boton2 = document.getElementById("btn2")
+const boton3 = document.getElementById("btn3")
+
+boton.addEventListener("click", () =>{
+    alert("Has seleccionado el curso de Html")
+})
+boton2.addEventListener("click", () =>{
+    alert("Has seleccionado el curso de Css")
+})
+boton3.addEventListener("click", () =>{
+    alert("Has seleccionado el curso de Javasript")
+})
